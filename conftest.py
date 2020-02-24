f99a2d39882d3031e0001d13f91e3b3d13e87955
Tomasz Cybal <t.cybal@cctechnology.pl>
2020-02-24 23:35:24 +0100

    TechTalk6: step #5

--- conftest.py
+++ conftest.py
@@ -1,9 +1,29 @@
+import subprocess
+import time
+from dotenv import load_dotenv
 import pytest
 from selenium import webdriver
 
 
+@pytest.yield_fixture(scope='session')
+def compose():
+    print('\nDocker compose up ...')
+    load_dotenv(dotenv_path='owncloud-docker-server/.env')
+    subprocess.run(['docker-compose', '-f', 'owncloud-docker-server/docker-compose.yml', 'up', '-d'],
+                   stdout=subprocess.PIPE,
+                   universal_newlines=True)
+    time.sleep(10)
+    yield
+
+    print('\nDocker compose down ...')
+    subprocess.run(['docker-compose', '-f', 'owncloud-docker-server/docker-compose.yml', 'down'],
+                   stdout=subprocess.PIPE,
+                   universal_newlines=True)
+    time.sleep(10)
+
+
 @pytest.yield_fixture(scope='function')
-def chrome_browser():
+def chrome_browser(compose):
     chrome_driver = webdriver.Chrome()
     chrome_driver.implicitly_wait(30)
     chrome_driver.get('http://localhost:8080')
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,25 +1,5 @@
-import subprocess
-import time
-from dotenv import load_dotenv
 from locators import OwnCloudLoginPageLocators
 from pages import LoginPage
-
-
-def docker_compose_up():
-    print('Docker compose up ...')
-    load_dotenv(dotenv_path='owncloud-docker-server/.env')
-    subprocess.run(['docker-compose', '-f', 'owncloud-docker-server/docker-compose.yml', 'up', '-d'],
-                   stdout=subprocess.PIPE,
-                   universal_newlines=True)
-    time.sleep(10)
-
-
-def docker_compose_down():
-    print('Docker compose down ...')
-    subprocess.run(['docker-compose', '-f', 'owncloud-docker-server/docker-compose.yml', 'down'],
-                   stdout=subprocess.PIPE,
-                   universal_newlines=True)
-    time.sleep(10)
 
 
 def test_login_correct_credentials(chrome_browser):
